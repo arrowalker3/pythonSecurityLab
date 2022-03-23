4e55c3f95bf4e6a8a7b3f8af5e8e97a14cc2bf76
arrowalker3 <arrowalker3@gmail.com>
2022-03-23 12:52:16 -0600

    first commit

---
+++ control.py
@@ -0,0 +1,32 @@
+########################################################################
+# COMPONENT:
+#    CONTROL
+# Author:
+#    Br. Helfrich, Kyle Mueller, <your name here if you made a change>
+# Summary:
+#    This class stores the notion of Bell-LaPadula
+########################################################################
+
+# enum of confidentiality levels
+import enum
+
+Level = {
+    'Public': 0,
+    'Confidential': 1,
+    'Privileged': 2,
+    'Secret': 3
+}
+
+def getLevel(stringLevel):
+    return Level[stringLevel]
+
+
+class Control:
+    def __init__(self):
+        pass
+
+    def securityConditionRead(self, assetControl, subjectControl):
+        return subjectControl >= assetControl
+
+    def securityConditionWrite(self, assetControl, subjectControl):
+        return subjectControl <= assetControl
